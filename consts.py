37dd5a207b2b2e684cd58cc3819d7fdfd8b889b8
akvachan <arsenii@kvachan.com>
2024-07-31 15:59:33 +0200

    updating and pushing last changes before colab replacement

--- consts.py
+++ consts.py
@@ -1,9 +1,27 @@
+import os
+
 # Name of the dataset
 DATASET_NAME = "food101"
 
+# Name of the base model
+BASE_MODEL_NAME = "ResNet-50"
+
 # Path to the dataset
-DATASET_PATH = getvar("DATASET_PATH")
+DATASET_PATH = "data"
+
+# Checkpoints of the base model
+CHECKPOINTS_BASE_PATH = "/tmp/ckpt/ResNet-50.keras"
+
+LOGS_PATH = "logs"
+
+# Path to the assets
+ASSETS_PATH = "assets"
+
+# Path to the assets
+IMAGES_PATH = os.path.join("assets", "images")
 
 # Target image shape
-IMAGE_SHAPE = (224, 224, 3)
+IMAGE_SHAPE = (512, 512, 3)
+
+LR = 0.0001
 
--- utils.py
+++ utils.py
@@ -1,3 +1,14 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+import datetime
+import functools
+import json
+import os
+import time
+from typing import Tuple
+
+
 # Time helper function
 def timer(func):
     """
@@ -14,7 +25,7 @@ def timer(func):
     return wrapper_timer
 
 
-def getvar(var: str, vars_file = os.path.join(".", ".credentials")) -> str:
+def getvar(var: str, vars_file=os.path.join(".", ".credentials")) -> str:
     """
     Credentials, secrets fetching function.
     """
@@ -24,22 +35,24 @@ def getvar(var: str, vars_file = os.path.join(".", ".credentials")) -> str:
 
 
 @timer
-def load(dataset_name: str, dataset_path: str) -> Tuple[tf.data.Dataset, tf.data.Dataset, tfds.core.DatasetInfo]:
-  """
-  Loads the specified dataset from TensorFlow Datasets.
-  """
-  (train_data, test_data), ds_info = tfds.load(
+def load(dataset_name: str, dataset_path: str) -> \
+        Tuple[tf.data.Dataset, tf.data.Dataset, tfds.core.DatasetInfo]:
+    """
+    Loads the specified dataset from TensorFlow Datasets.
+    """
+    (test_set, val_set, train_set), ds_info = tfds.load(
       name=dataset_name,
       data_dir=dataset_path,
-      split=["train", "validation"],
+      split=["validation", "train[0%:15%]", "train[15%:]"],
       shuffle_files=True,
       as_supervised=True,
       with_info=True
-  )
-  return train_data, test_data, ds_info
+    )
+    return test_set, val_set, train_set, ds_info
 
 
-def random_sample(data: tf.data.Dataset, ds_info: tfds.core.DatasetInfo) -> dict:
+def random_sample(data: tf.data.Dataset, ds_info: tfds.core.DatasetInfo) -> \
+        dict:
     """
     Selects one random sample with all important information saved as dict.
     """
@@ -50,62 +63,107 @@ def random_sample(data: tf.data.Dataset, ds_info: tfds.core.DatasetInfo) -> dict
         image_info["tensor"] = image
         image_info["content"] = show_image(image)
         image_info["label_tensor"] = label
-        image_info["label_str"] = ds_info.features["label"].names[label.numpy()]
-        image_info["tensor_range"] = (tf.reduce_min(image), tf.reduce_max(image))
+        image_info["label_str"] = (
+                ds_info
+                .features["label"]
+                .names[label.numpy()]
+        )
+        image_info["tensor_range"] = (
+                tf.reduce_min(image),
+                tf.reduce_max(image)
+        )
     return image_info
 
 
 def show_image(image_tensor: tf.Tensor):
-  """
-  Plots an image from a tensor.
-  """
-  image_numpy = image_tensor.numpy()
-  plt.imshow(image_numpy)
-  plt.axis('off')
-
-
-def prepare_dataset(dataset: tf.data.Dataset):
     """
-    Dataset pipeline set-up. Preprocessing images with the preprocess_image function, batching them and optimzing the pipeline via prefetching.
+    Plots an image from a tensor.
     """
-    for subset in dataset:
-      subset = (
-          subset
-          .map(preprocess_image, num_parallel_calls=tf.data.AUTOTUNE)
-          .batch(batch_size=64)
-          .prefetch(buffer_size=tf.data.AUTOTUNE)
-      )
+    image_numpy = image_tensor.numpy()
+    plt.imshow(image_numpy)
+    plt.axis('off')
 
 
-def preprocess_image(image: tf.Tensor, label: str, img_shape=224) -> Tuple[tf.Tensor, str]:
-  """
-  Converts image tensor from uint8 (or any) to float32 tensor, then reshapes to [img_shape, img_shape, colour_channels].
-  """
-  image = tf.image.resize(image, [img_shape, img_shape])
-  return tf.cast(image, tf.float32), label
+def prepare_dataset(dataset: tf.data.Dataset, img_shape=512):
+    """
+    Dataset pipeline set-up. Preprocessing images with the preprocess_image \
+            function, batching them and optimzing the pipeline via prefetching.
+    """
+    dataset = (
+        dataset
+        .map(functools.partial(preprocess_image, img_shape=img_shape), \
+             num_parallel_calls=tf.data.AUTOTUNE)
+        .shuffle(buffer_size=1000)
+        .batch(batch_size=128, drop_remainder=True)
+        .prefetch(buffer_size=tf.data.AUTOTUNE)
+    )
+    return dataset
+
+
+def preprocess_image(image: tf.Tensor, label: str, img_shape=512) \
+        -> Tuple[tf.Tensor, str]:
+    """
+    Converts image tensor from uint8 (or any) to float32 tensor, \
+    then reshapes to [img_shape, img_shape, colour_channels].
+    """
+    image = tf.image.resize(image, [img_shape, img_shape])
+    return tf.cast(image, tf.float32), label
 
 
 def create_tensorboard_callback(dir_name, model_name):
     """
-    Creates a TensorBoard callback instance under dir_name/model_name/current_datetime/ to store log files
+    Creates a TensorBoard callback instance under \
+            dir_name/model_name/current_datetime/ to store log files
     """
-    log_dir = os.path.join(dir_name, model_name, datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
-
-    print("Saving TensorBoard log files...")
+    log_dir = os.path.join(
+            dir_name,
+            model_name,
+            datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+            )
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
-    print(f"TensorBoard log files saved to {str(log_dir)}.")
 
     return tensorboard_callback
 
+
 def create_checkpoint_callback(checkpoint_path, monitor="val_acc"):
     """
-    Creates a checkpoint callback (saves best model during training) under checkpoint path
+    Creates a checkpoint callback (saves best model during training) \
+            under checkpoint path
     """
-    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
-                                                             monitor=monitor,
-                                                             save_best_only=True,
-                                                             verbose=1)
+    checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+            checkpoint_path,
+            monitor=monitor,
+            save_best_only=True,
+            verbose=1
+            )
     return checkpoint_callback
 
-def early_stopping_callbakc():
-    return None
+
+def create_early_stopping_callback():
+    early_stopping_callback = tf.keras.callbacks.EarlyStopping(
+        monitor='val_loss',
+        patience=3,
+        verbose=1,
+        mode='min',
+        restore_best_weights=True,
+        start_from_epoch=2
+    )
+    return early_stopping_callback
+
+    
+class AntiOverfittingCallback(tf.keras.callbacks.Callback): 
+    """
+    Halt the training when validation accuracy is \
+        eps lower than the training accuracy
+    """
+    def __init__(self):
+        self.eps = 0.07
+        super().__init__()
+            
+    def on_epoch_end(self, epoch, logs={}): 
+        val_acc = logs.get('val_sparse_categorical_accuracy')
+        train_acc = logs.get('sparse_categorical_accuracy')
+        if train_acc - val_acc > self.eps:
+            print(f"val_acc {val_acc} is {self.eps} lower than acc {train_acc} \
+                    -> stopping training at epoch {epoch}")   
+            self.model.stop_training = True
